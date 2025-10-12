75074be8f2c3c94361ff2911849ba13a3e8f2b89
Reza Raka Nugraha <rezaraka00@gmail.com>
2025-10-12 20:08:19 +0700

    Remove standalone output configuration from next.config.js

--- next.config.js
+++ next.config.js
@@ -17,7 +17,6 @@ const nextConfig = {
   //   styledComponents: true,
   // },
   reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
-  output: 'standalone',
   images: {},
   webpack(config, { isServer }) {
     if (!isServer) {
